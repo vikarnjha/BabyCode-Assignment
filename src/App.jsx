e587853c840c084b0700d5d04f1a248d80466839
Vikarn Jha <vikarnjha91@gmail.com>
2025-05-07 23:22:42 +0530

    fix App rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import {} from "react";
 import Auth from "./components/Auth";
 import "./App.css";
-import Signup from "./components/SignUp";
 
 function App() {
   return (
     <>
-      {/* <Auth /> */}
-      <Signup/>
+      <Auth />
     </>
   );
 }
